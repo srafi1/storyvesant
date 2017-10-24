d55b8d5ef035f588f089f4b6dad6fa838f10b443
bleung329 <bleung@stuy.edu>
2017-10-24 13:17:27 -0400

    working on auth function

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 #!usr/bin/python
 from flask import Flask, session, render_template, request, redirect, url_for, flash
-import os
+import os, sqlite3
 
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+def auth(user,passo,upass):
+	'''
+	stat_code -1 = bad username
+	stat_code 0 = bad password
+	stat_code 1 = good
+	'''
+	stat_code = -1
+	if user in upass:
+		stat_code+=1
+		if passo == upass[user]:
+			stat_code+=1
+	return stat_code
+
+
+def auth(usar,passwad):
+	db_name = "upass.db"
+	cmd = ""
+	if "user" in request.args and "passo" in request.args:
+		username = request.args["user"]
+		password = request.args["passo"]
+
+def register(usar, passwad):
 
 @app.route("/")
 def landing():
