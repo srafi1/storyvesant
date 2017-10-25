0f4819e9b29aaf1b3d6e45cfa00f05ad97b4739c
srafi1 <shakilrafi0@gmail.com>
2017-10-25 14:07:52 -0400

    register

--- app.py
+++ app.py
@@ -8,21 +8,38 @@ app.secret_key = os.urandom(32)
 
 @app.route("/")
 def landing():
-	print "login status code test: "+str(loggit.auth("admin","password"));
-	loggit.register("a","admin2")
-	return render_template("index.html", title = "Welcome")
+    #print "login status code test: "+str(loggit.auth("admin","password"));
+    #loggit.register("a","admin2")
+    return render_template("index.html", title = "Welcome")
 
 @app.route("/login")
 def login_route():
     return render_template("login.html", title = "Login")
 
-@app.route("/register")
+@app.route("/register", methods = ["GET", "POST"])
 def register_route():
-
+    if request.method == "POST":
+        uname = request.form.get("uname") 
+        pword = request.form.get("pword") 
+        pword2 = request.form.get("pword2") 
+        valid = True
+        if uname == None or uname == "":
+            flash("Enter a username")
+            valid = False
+        if pword == None or pword == "":
+            flash("Enter a password")
+            valid = False
+        if pword != pword2:
+            flash("Passwords do not match")
+            valid = False
+        if valid:
+            loggit.register(uname, pword)
+            return redirect("/login")
     return render_template("register.html", title = "Register")
 
-#@app.route("/read")
+#@app.route("/read/<int:id>")
 #def read():
+    
 
 #@app.route("/edit")
 
